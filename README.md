17709292ab162e7fc085d401c0b983ec75a7396a
snayder15 <jhumpiri1599@gmail.com>
2019-09-01 20:17:56 -0300

    subiendo la resolucion del practico 0

--- README.md
+++
@@ -1,1 +0,0 @@
-# Ing-Sotf-1-ejercicios
---
+++ ToTheMoon-00/Practico0_ejercicio1.cpp
@@ -0,0 +1,51 @@
+/*Ejercicio 1*/
+
+#define  velocidadSueloCompacto 150;
+#define  tiempoSueloCompacto 10; 
+#define  velociadSueloPoroso 100;
+#define  tiempoSueloPoroso 5; 
+int objeto;
+
+
+void tomarObjetoEnSueloCompacto(){
+	
+	girarMechaEnSentidoReloj(tiempoSueloCompacto,velocidadSueloCompacto);
+	cerramosLaPinsas(objeto);
+	girarMechaEnSentidoContrarioReloj(tiempoSueloCompacto,velocidadSueloCompacto);
+}
+
+void tomarObjetoEnSueloPoroso(){
+	girarMechaEnSentidoContrarioReloj(tiempoSueloPoroso,velocidadSueloPoroso);
+	cerramosLaPinsas(objeto);
+	girarMechaEnSentidoReloj(tiempoSueloPoroso,velocidadSueloPoroso);
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+	int dureza = argv[1];
+	int porosidad = argv[2];
+	
+	/*Aqui una desicion que tomamos es:
+		Suelo duro compacto : dureza > 0 y porosidad = 0
+		Suelo suave poroso  : dureza = 0 y porosidad > 0
+		Si dureza > 0 y porosidad > 0 entonces el robot no ejecuta accion
+		Si dureza = 0 y porosidad = 0 entonces el robot no ejecuta accion
+	*/
+	
+	if((dureza = 0 && porosidad = 0) || (dureza > 0 && porosidad > 0)){
+		exit(0);
+	}
+	
+	if(dureza > 0 && porosidad = 0){
+		tomarObjetoEnSueloCompacto();
+	}
+	
+	if(dureza = 0 && porosidad > 0){
+		tomarObjetoEnSueloPoroso();
+	}
+	
+	return 0;
+}
+
